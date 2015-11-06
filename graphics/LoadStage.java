54ac375982b7e1973af90fb1f4d343d3a694f04e
Lucho <lucianogbianchi@gmail.com>
2015-11-05 22:54:54 -0300

    CheckBoxes para 1 o 2 jugadores

--- graphics/LoadStage.java
+++ graphics/LoadStage.java
@@ -9,6 +9,7 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 
@@ -21,7 +22,10 @@ public class LoadStage extends Application {
     Canvas[] defaultPreviews = new Canvas[3];
     Button[] defaultButtons = new Button[3];
     Button loadBtn;
-
+    CheckBox twoPlayers;
+    CheckBox onePlayer;
+    TextField nameInput;
+    
     final int BUTTON_WIDTH = 100;
     final int BUTTON_HEIGHT = 35;
     final int PREVIEW_WIDTH = 200;
@@ -31,8 +35,9 @@ public class LoadStage extends Application {
     public void start(final Stage loadStage) throws Exception {
         Group root = new Group();
         loadStage.setWidth(800);
-        loadStage.setHeight(400);
+        loadStage.setHeight(450);
 
+        //Botones configuraciones iniciales
         for (int i = 0; i < 3; i++) {
             defaultPreviews[i] = new Canvas(PREVIEW_WIDTH, PREVIEW_HEIGHT);
             defaultPreviews[i].setTranslateX(50 * (i + 1) + PREVIEW_WIDTH * i);
@@ -40,7 +45,7 @@ public class LoadStage extends Application {
             defaultPreviews[i].getGraphicsContext2D()
                     .drawImage(new Image("file:assets/DefaultPreview" + (i + 1) + ".png"), 0, 0);
         }
-
+        
         defaultButtons[0] = new Button("Tutankamon");
         defaultButtons[1] = new Button("Ramses");
         defaultButtons[2] = new Button("Cleopatra");
@@ -49,22 +54,47 @@ public class LoadStage extends Application {
             defaultButtons[i].setTranslateY(225);
             defaultButtons[i].setPrefSize(BUTTON_WIDTH, BUTTON_HEIGHT);
         }
-
+        
+        //Boton de carga partida guardada
         loadBtn = new Button("Load Game");
         loadBtn.setTranslateX(175);
-        loadBtn.setTranslateY(315);
+        loadBtn.setTranslateY(450-85);
         loadBtn.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGHT);
-        final TextField nameInput = new TextField();
+        
+        //Texto para el nombre de archivo a cargar
+        nameInput = new TextField();
         nameInput.setPrefSize(350, BUTTON_HEIGHT);
         nameInput.setTranslateX(275);
-        nameInput.setTranslateY(315);
-
+        nameInput.setTranslateY(450-85);
+        
+        //Checkboxes cantidad de jugadores
+        twoPlayers = new CheckBox("Dos Jugadores");
+        onePlayer = new CheckBox("Un Jugador");
+        twoPlayers.setTranslateX(250); onePlayer.setTranslateX(450);
+        twoPlayers.setTranslateY(450-85-50); onePlayer.setTranslateY(450-85-50);
+        twoPlayers.setSelected(true); // Por default 2 jugadores
+        
+        //Para que al seleccionar uno se deseleccione el otro
+        onePlayer.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                onePlayer.setSelected(true);
+                twoPlayers.setSelected(false);
+            }  
+        });
+        
+        twoPlayers.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                twoPlayers.setSelected(true);
+                onePlayer.setSelected(false);
+            }            
+        });
+        
         // Ver el tema de un for{}
-
+        //Acciones Botones Config Default
         defaultButtons[0].setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent e) {
                 try {
-                    new BoardStage("defaultConfigs/def1", GameMode.PVP, loadStage);
+                    new BoardStage("defaultConfigs/def1", getSelectedGameMode(), loadStage);
                 } catch (Exception e1) {
                     throwUnexpectedErrorAlert();
                 }
@@ -75,7 +105,7 @@ public class LoadStage extends Application {
         defaultButtons[1].setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent e) {
                 try {
-                    new BoardStage("defaultConfigs/def2", GameMode.PVP, loadStage);
+                    new BoardStage("defaultConfigs/def2", getSelectedGameMode(), loadStage);
                 } catch (Exception e1) {
                     throwUnexpectedErrorAlert();
                 }
@@ -86,26 +116,22 @@ public class LoadStage extends Application {
         defaultButtons[2].setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent e) {
                 try {
-                    new BoardStage("defaultConfigs/def3", GameMode.PVP, loadStage);
+                    new BoardStage("defaultConfigs/def3", getSelectedGameMode(), loadStage);
                 } catch (Exception e1) {
                     throwUnexpectedErrorAlert();
                 }
                 loadStage.hide();
             }
         });
-
+        
+        //Accion boton de cargar juego guardado
         loadBtn.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent e) {
                 try {
                     new BoardStage("savedGames/" + nameInput.getText(), loadStage);
                     loadStage.close();
                 } catch (IOException fileNotFound) {
-                    Alert notfound = new Alert(Alert.AlertType.ERROR);
-                    notfound.setTitle("File Not Found");
-                    notfound.setHeaderText(null);
-                    notfound.setContentText("File: '" + nameInput.getText()
-                            + "' was not found.\nPlease check if the filename is correct");
-                    notfound.showAndWait();
+                    throwNotFoundFileAlert();
                 } catch (ClassNotFoundException classNotFound) {
                     throwInvalidFileAlert();
                 } catch (Exception e1){
@@ -119,6 +145,8 @@ public class LoadStage extends Application {
         root.getChildren().addAll(defaultButtons);
         root.getChildren().add(loadBtn);
         root.getChildren().add(nameInput);
+        root.getChildren().add(onePlayer);
+        root.getChildren().add(twoPlayers);
 
         loadStage.setResizable(false);
         loadStage.setTitle("Khet - Board Selection");
@@ -126,6 +154,25 @@ public class LoadStage extends Application {
         loadStage.show();
     }
 
+    private GameMode getSelectedGameMode(){
+        if (onePlayer.isSelected()) {
+            return GameMode.PVE;
+        }
+        if (twoPlayers.isSelected()) {
+            return GameMode.PVP;
+        }
+        throw new IllegalStateException("Unselected GameMode");
+    }
+    
+    private void throwNotFoundFileAlert() {
+        Alert notfound = new Alert(Alert.AlertType.ERROR);
+        notfound.setTitle("File Not Found");
+        notfound.setHeaderText(null);
+        notfound.setContentText("File: '" + nameInput.getText()
+        + "' was not found.\nPlease check if the filename is correct");
+        notfound.showAndWait();                    
+    }
+    
     /**
      * Se emite una alerta en caso de que el archivo que se desea abrir exista pero no corresponda a un
      * archivo valido de Keth Game
@@ -149,6 +196,7 @@ public class LoadStage extends Application {
         alert.setContentText("There was an unexpected error loading the file.\nPlease try again");
         alert.showAndWait();
     }
+   
     public static void main(String[] args) {
         launch(args);
     }
