1acc4c7ee25c532e3994b7e89ded1f4f3b566b6d
tcerdaITBA <tcerda@itba.edu.ar>
2015-11-05 22:47:27 -0300

    Ahora van los cañones en serio

---
+++ poo/khet/RedCannon.java
@@ -0,0 +1,12 @@
+package poo.khet;
+
+import poo.khet.gameutils.Direction;
+
+public class RedCannon extends BeamCannon {
+
+	private static final long serialVersionUID = 1L;
+
+	public RedCannon() {
+		super(Direction.NORTH);
+	}
+}
---
+++ poo/khet/SilverCannon.java
@@ -0,0 +1,12 @@
+package poo.khet;
+
+import poo.khet.gameutils.Direction;
+
+public class SilverCannon extends BeamCannon {
+
+	private static final long serialVersionUID = 1L;
+	
+	public SilverCannon() {
+		super(Direction.SOUTH);
+	}
+}
