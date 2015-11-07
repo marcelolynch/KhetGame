f7b5d2064eb2b2e34e84d6c11a8a8e6778bf3cc1
Carla Barruffaldi <carla_barru@yahoo.com.ar>
2015-11-07 15:27:48 -0300

    Documentacion Position
    Esta tratado como (fila, columna)

--- poo/khet/gameutils/Position.java
+++ poo/khet/gameutils/Position.java
@@ -2,31 +2,65 @@ package poo.khet.gameutils;
 
 import java.io.Serializable;
 
+/**
+ * Representa una ubicacion respecto de un <code>Board</code> mediante coordenadas que
+ * indican Fila y Columna.
+ * <p>
+ * Nota: La posición (0,0) es aquella que se encuentra en el extremo superior izquierdo de un Board.
+ * @see poo.khet.Board
+ */
 public class Position implements Serializable {
 
     private static final long serialVersionUID = 1L;
-
+    /**
+     * Numero de fila
+     */
     private int row;
+
+    /**
+     * Numero de columna
+     */
     private int col;
 
+    /**
+     * Crea una nueva posicion a partir de un numero de fila y columna dados.
+     * @param row Numero de fila
+     * @param col Numero de columna
+     */
     public Position(int row, int col) {
         this.row = row;
         this.col = col;
     }
 
+    /**
+     * Crea la posicion (0,0)
+     */
     public Position() {
         this(0, 0);
     }
 
+    /**
+     * Devuelve el numero de la fila
+     * @return el numero de la fila
+     */
     public int getRow() {
         return row;
     }
 
+    /**
+     * Devuelve el numero de la columna
+     * @return el numero de la Columna
+     */
     public int getCol() {
         return col;
     }
 
-
+    /**
+     * Verifica si una posicion es adyacente a otra, es decir si se encuentra
+     * a una unidad (fila o columna) de distancia.
+     * @param otherPos Posicion respecto de la cual se quiere comparar
+     * @return <b><tt>true</tt></b> si la posicion es adyacente, <b><tt>false</tt></b> en caso contrario.
+     */
     public boolean isAdjacent(Position otherPos) {
         if (this.equals(otherPos)) {
             return false;
@@ -39,10 +73,10 @@ public class Position implements Serializable {
     }
 
     /**
-     * Devuelve la coordenada que se encuentra a una unidad de distancia segun la direccion
+     * Devuelve la coordenada que se encuentra a una unidad (fila o columna) de distancia segun la direccion
      * especificada.
      * 
-     * @param direction - Direccion en la cual cambia la coordenada
+     * @param direction Direccion en la cual cambia la coordenada
      */
     public Position getPosInDirection(Direction direction) {
         if (direction == null)
