d3d1fe17e233751777fa253fb22758eaecce3035
marcelolynch <marcelomlynch@gmail.com>
2015-11-08 00:09:16 -0300

    Documentacion: BeamFate

--- poo/khet/BeamFate.java
+++ poo/khet/BeamFate.java
@@ -1,5 +1,18 @@
 package poo.khet;
 
+/**
+ * BeamFate representa el destino que tuvo un rayo en su recorrido por el
+ * tablero, dado por el manejo en {@link BeamManager}
+ * 
+ * @see BeamManager
+ */
 public enum BeamFate {
-    IMPACTED_PIECE, OUT_OF_BOUNDS, WAS_CONTAINED;
+    /** El rayo alcanz&oacute; una pieza y logr&oacute; impactarla */
+	IMPACTED_PIECE, 
+    
+	/** El rayo termin&oacute; por irse de los l&iacute;mites del tablero */
+	OUT_OF_BOUNDS, 
+    
+	/** El rayo fue desactivado por alg&uacute;n accesorio de alguna pieza */
+	WAS_CONTAINED;
 }
