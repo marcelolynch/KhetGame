20db0ea39f0af37b6f6e98fca143bc84d4665757
Carla Barruffaldi <carla_barru@yahoo.com.ar>
2015-11-08 13:45:08 -0300

    Doc BeamCannon
    Si a alguno se le ocurre otro sustantivo en vez de “elemento” que
    describa mejor lo cambia….

--- poo/khet/BeamCannon.java
+++ poo/khet/BeamCannon.java
@@ -4,11 +4,28 @@ import java.io.Serializable;
 
 import poo.khet.gameutils.Direction;
 
+/**
+ * Un {@link BeamCannon} es un elemento de un {@link Game} que emite un {@link Beam}
+ * en una {@link Direction}, dependiendo de su orientación.
+ * <p>
+ * Cada {@link BeamCannon} solo tiene dos posibles {@link Direction}s a las cuales estar orientado.
+ * @see Beam
+ * @see CannonPositions
+ * @see Direction
+ *
+ */
 public abstract class BeamCannon implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    /**
+     * Indica la orientacion del cañón
+     */
     private Direction facing;
+
+    /**
+     * Indica si debe alternarse su orientacion en sentido horario o antihorario.
+     */
     private boolean clockwiseSwitch;
 
     public BeamCannon(Direction direction) {
@@ -16,16 +33,27 @@ public abstract class BeamCannon implements Serializable {
         facing = direction;
     }
 
+    /**
+     * Devuelve hacia adonde esta orientado el {@link BeamCannon}
+     * @return <code>Direction</code> a la cual esta orientado
+     */
     public Direction getFacing() {
         return facing;
     }
 
+    /**
+     * Genera un nuevo {@link Beam} en una <code>Direction</code> que coincide
+     * con la dirección hacia donde esta orientado el cañon.
+     * @return el <code>Beam</code> generado
+     * @see #getFacing()
+     */
     public Beam generateBeam() {
         return new Beam(getFacing());
     }
 
     /**
-     * Alterna la orientación del laser (entre las dos posibles)
+     * Alterna la orientación del cañon (entre las dos posibles)
+     * @see Direction
      */
     public void switchFacing() {
         if (clockwiseSwitch) {
