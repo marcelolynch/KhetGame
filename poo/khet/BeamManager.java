7a809c7732ff09506c1f21fc42b823016c93b319
marcelolynch <marcelomlynch@gmail.com>
2015-11-08 00:26:23 -0300

    Documentacion: BeamManager

--- poo/khet/BeamManager.java
+++ poo/khet/BeamManager.java
@@ -5,10 +5,10 @@ import java.util.List;
 
 import poo.khet.gameutils.Position;
 /**
- * {@code BeamManager} lleva a cabo el lanzamiento de un <code>Beam</code> en un tablero dado, indiferentemente del equipo que lo genere.
+ * {@code BeamManager} lleva a cabo el lanzamiento y seguimiento de <code>Beam</code>s sobre un tablero dado,
+ * y su interacci&oacute;n con las piezas que se encuentren en el mismo.
  *
- * Lleva un registro del recorrido que siguió éste <code>Beam</code>
- * 
+ * Lleva un registro del recorrido que siguió el &uacute;ltimo <code>Beam</code> manejado.
  * 
  * @see {@link Beam}
  */
@@ -16,6 +16,12 @@ public class BeamManager {
     private Board board;
     private List<Position> beamTrace = new ArrayList<>();
 
+    /**
+     * Construye un nuevo <code>BeamManager</code> que trabajar&aacute; sobre el tablero 
+     * que se pasa por par&aacute;metro.
+     * 
+     * @param board - el tablero sobre el que se manejar&aacute;n los rayos
+     */
     public BeamManager(Board board) {
         super();
         this.board = board;
@@ -30,18 +36,22 @@ public class BeamManager {
     }
     
     /**
+     * Devuelve una lista con las posiciones que ocup&oacute; el ultimo rayo emitido
+     * en su trayectoria por el tablero
      * 
-     * @return el recorrido del último <code>Beam</code> beam lanzado.
+     * @return el recorrido del último <code>Beam</code> lanzado.
      */
-    
     public List<Position> getBeamTrace() {
         return beamTrace;
     }
+    
     /**
-     * Lanza el <code>Beam</code> beam y sigue su recorrido.
-     * @param beam
-     * @param initialPosition
-     * @return El resultado obtenido en ese recorrido.
+     * Lanza el <code>Beam</code> al tablero y sigue su recorrido.
+     * @param beam - el rayo a manejar
+     * @param initialPosition - la primera posici&oacute;n del rayo en el tablero 
+     * @return - el resultado obtenido en ese recorrido.
+     * 
+   	 * @see BeamFate
      */
     public BeamFate manageBeam(Beam beam, Position initialPosition) {
         BeamFate beamAction = null;
@@ -66,11 +76,14 @@ public class BeamManager {
 
         return beamAction;
     }
+
     /**
-     *  
-     * @param pos
-     * @param beam
-     * @return la <code>Position</code> siguiente a la que debería ir el <code>Beam</code> .
+     * Calcula la siguiente {@link Position} que debe ocupar el rayo indicado, a partir de 
+     * la posicion que se indica por par&aacute;metro
+     * @param pos - la posicion desde la que se quiere calcular 
+     * @param beam - el rayo sobre el que se realiza el calculo
+     * 
+     * @return la <code>Position</code> siguiente a la que debería ir el <code>Beam</code>
      */
     private Position nextBeamPosition(Position pos, Beam beam) {
         return pos.getPosInDirection(beam.getDirection());
