b6e8f0c70f8af25c95ea7167db29d4f670fbbfb7
marcelolynch <marcelomlynch@gmail.com>
2015-11-07 23:49:28 -0300

    Documentacion: Beam

--- poo/khet/Beam.java
+++ poo/khet/Beam.java
@@ -2,22 +2,43 @@ package poo.khet;
 
 import poo.khet.gameutils.Direction;
 
+/**
+ * Un <code>Beam</code> es una entidad que, de estar activo, lleva una direcci&oacute;n
+ * de movimiento. El rayo puede ser desactivado, en cuyo caso pierde la direcci&oacute;n
+ * 
+ * @see Piece
+ */
 public class Beam {
     /**
-     * Indica si el rayo esta vivo
+     * Indica si el rayo esta activo
      */
     private boolean isActive;
+    
+    /**
+     * Indica la direcci&oacute;n de viaje del rayo
+     */
     private Direction direction;
 
+    /**
+     * Construye un nuevo rayo activo, con la direcci&oacute;n de viaje especificada
+     * @param initialDir
+     */
     Beam(Direction initialDir) {
         isActive = true;
         direction = initialDir;
     }
 
+    /**
+     * Desactiva el rayo
+     */
     void deactivate() {
         isActive = false;
     }
 
+    /**
+     * Indica si el rayo est&aacute; activo
+     * @return <code>true</code> si el rayo esta vivo, <code>false</code> de lo contrario
+     */
     public boolean isActive() {
         return isActive;
     }
@@ -36,9 +57,9 @@ public class Beam {
     }
 
     /**
-     * Cambia la dirección del rayo
+     * Cambia la direcci&oacute;n actual del rayo por la direcci&oacute;n especificada
      * 
-     * @param newDirection - nueva dirección del rayo
+     * @param newDirection - nueva direcci&oacute;n del rayo
      * @throws IllegalStateException si el rayo no se encuentra activo
      */
     void setDirection(Direction newDirection) {
